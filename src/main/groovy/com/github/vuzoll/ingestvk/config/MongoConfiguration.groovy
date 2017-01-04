c6a1da763136fa20b6f1a32ccb263391b44dacbc
Yaroslav Yermilov <yermilov17@gmail.com>
2017-01-04 14:47:25 +0200

    #12. fix several auth issues

--- src/main/groovy/com/github/vuzoll/ingestvk/config/MongoConfiguration.groovy
+++ src/main/groovy/com/github/vuzoll/ingestvk/config/MongoConfiguration.groovy
@@ -13,7 +13,7 @@ class MongoConfiguration extends AbstractMongoConfiguration {
     static String DATABASE_NAME = System.getenv('INGEST_VK_MONGO_DATABASE_NAME') ?: 'vkIngested'
     static String HOST = System.getenv('INGEST_VK_MONGO_HOST') ?: 'vuzoll_mongo'
     static Integer PORT = System.getenv('INGEST_VK_MONGO_PORT') ? Integer.parseInt(System.getenv('INGEST_VK_MONGO_PORT')) : 27017
-    static String USERNAME = System.getenv('INGEST_VK_MONGO_USERNAME') ?: 'vuzollAdmin'
+    static String USERNAME = System.getenv('INGEST_VK_MONGO_USERNAME') ?: 'ingestVkService'
     static String PASSWORD = System.getenv('INGEST_VK_MONGO_PASSWORD')
 
     @Override
